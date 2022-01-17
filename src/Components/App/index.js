0531052aed79090c5d5601801a6780ee7eac1cf0
Graham <graycanny@hotmail.com>
2022-01-17 16:40:47 +0000

    Working on welcome page - G

--- src/Components/App/index.js
+++ src/Components/App/index.js
@@ -2,17 +2,25 @@ import "./App.css";
 import WelcomePage from "../WelcomePage";
 import Input from "../Input";
 import Display from "../Display";
+import { useState } from "react";
 
 function App() {
-  //
+  const [welcome, setWelcome] = useState(false);
+
+  if (welcome)
+    return (
+      <div className="parent-div">
+        <Input />
+        <Display />
+      </div>
+    );
+
   return (
     <div className="App">
-      <WelcomePage />
+      <WelcomePage setWelcome={setWelcome} />
       <h1>
         Input and display will be hidden until welcome page has been passed.
       </h1>
-      <Input />
-      <Display />
     </div>
   );
 }
--- src/Components/WelcomePage/index.js
+++ src/Components/WelcomePage/index.js
@@ -1,11 +1,22 @@
 import React from "react";
+import { BsArrowRightSquare } from "react-icons/bs";
 
-function WelcomePage() {
+function WelcomePage({ setWelcome }) {
+  function handleClick(e) {
+    console.log("ONCLICK FUNCTION");
+    setWelcome(true);
+  }
+
   return (
     <div>
-      <h1>Welcome...</h1>
-      <h2>Please enter your name...</h2>
+      <h1>Hey there, Camper!</h1>
+      <h2>What shall I call you?</h2>
       <input type="text" placeholder="Name..." />
+      <BsArrowRightSquare
+        onClick={(e) => {
+          handleClick(e);
+        }}
+      />
     </div>
   );
 }
