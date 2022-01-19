c09fcb46dd2917c242193064011af829e6b0c2fc
Graham <graycanny@hotmail.com>
2022-01-19 13:58:23 +0000

    Deploy refactor

--- src/Components/Display/index.js
+++ src/Components/Display/index.js
@@ -1,33 +1,33 @@
-import React, {useEffect, useState} from 'react'
-import './Display.css'
-const url = 'http://localhost:3005'
+import React, { useEffect, useState } from "react";
+import "./Display.css";
+const url = "http://localhost:3005";
 
-function Display({userTable}) {
-	const [progTable, setProgTable] = useState({})
+function Display({ userTable }) {
+  const [progTable, setProgTable] = useState({});
 
-	async function fetchProgressTable() {
-		try {
-			const response = await fetch(`${url}/progress`)
-			const data = await response.json()
-			console.log('progress table data: ', data)
-			setProgTable(data)
-			console.log('progress table state: ', progTable)
-		} catch (error) {
-			console.log(error.message)
-		}
-	}
+  async function fetchProgressTable() {
+    try {
+      const response = await fetch(`${url}/progress`);
+      const data = await response.json();
+      console.log("progress table data: ", data);
+      setProgTable(data);
+      console.log("progress table state: ", progTable);
+    } catch (error) {
+      console.log(error.message);
+    }
+  }
 
-	useEffect(() => {
-		fetchProgressTable()
-	}, [])
+  useEffect(() => {
+    fetchProgressTable();
+  });
 
-	console.log('this is the user table', userTable)
+  console.log("this is the user table", userTable);
 
-	return (
-		<div>
-			<h1>Here is where the data will be displayed using reactChart</h1>
-		</div>
-	)
+  return (
+    <div>
+      <h1>Here is where the data will be displayed using reactChart</h1>
+    </div>
+  );
 }
 
-export default Display
+export default Display;
