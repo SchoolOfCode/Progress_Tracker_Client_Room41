556e92fd7e8119ad0025cad5cf56d34fb76b8e0c
21ciprian <2021ciprianoffice@gmail.com>
2022-01-19 20:38:06 +0000

    Moved async functions inside useEffect and added missing dependency arrays

--- src/Components/Display/index.js
+++ src/Components/Display/index.js
@@ -1,33 +1,32 @@
-import React, { useEffect, useState } from "react";
-import "./Display.css";
-const url = process.env.REACT_APP_API_URL;
+import React, {useEffect, useState} from 'react'
+import './Display.css'
+const url = process.env.REACT_APP_API_URL
 
-function Display({ userTable }) {
-  const [progTable, setProgTable] = useState({});
+function Display({userTable}) {
+	const [progTable, setProgTable] = useState({})
 
-  async function fetchProgressTable() {
-    try {
-      const response = await fetch(`${url}/progress`);
-      const data = await response.json();
-      console.log("progress table data: ", data);
-      setProgTable(data);
-      console.log("progress table state: ", progTable);
-    } catch (error) {
-      console.log(error.message);
-    }
-  }
+	useEffect(() => {
+		async function fetchProgressTable() {
+			try {
+				const response = await fetch(`${url}/progress`)
+				const data = await response.json()
+				console.log('progress table data: ', data)
+				setProgTable(data)
+			} catch (error) {
+				console.log(error.message)
+			}
+		}
+		fetchProgressTable()
+	}, [])
+	console.log('progress table state: ', progTable)
 
-  useEffect(() => {
-    fetchProgressTable();
-  });
+	console.log('this is the user table', userTable)
 
-  console.log("this is the user table", userTable);
-
-  return (
-    <div>
-      <h1>Here is where the data will be displayed using reactChart</h1>
-    </div>
-  );
+	return (
+		<div>
+			<h1>Here is where the data will be displayed using reactChart</h1>
+		</div>
+	)
 }
 
-export default Display;
+export default Display
