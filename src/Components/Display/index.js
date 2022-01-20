d50337116b00b3f5264e6e6e72e6b55f3941217e
Graham <graycanny@hotmail.com>
2022-01-20 09:59:35 +0000

    react-chart added and line graph addedd to Display

--- src/Components/Display/index.js
+++ src/Components/Display/index.js
@@ -1,9 +1,30 @@
 import React, { useEffect, useState } from "react";
+import { Doughnut, Line } from "react-chartjs-2";
 import "./Display.css";
 const url = process.env.REACT_APP_API_URL || "http://localhost:3005";
 
 function Display({ userTable }) {
   const [progTable, setProgTable] = useState({});
+  const [chartData, setChartData] = useState({});
+
+  setChartData({
+    data: {
+      labels: ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"],
+      datasets: [
+        {
+          label: "Score",
+          data: ["5", "11", "8", "7", "9"],
+          backgroundColor: [
+            "#0099ff",
+            "#4292c8",
+            "#2f6081",
+            "#f6b93b",
+            "#e58e26",
+          ],
+        },
+      ],
+    },
+  });
 
   useEffect(() => {
     async function fetchProgressTable() {
@@ -25,6 +46,9 @@ function Display({ userTable }) {
   return (
     <div>
       <h1>Here is where the data will be displayed using reactChart</h1>
+      <div className="chart">
+        <Line data={chartData} />
+      </div>
     </div>
   );
 }
